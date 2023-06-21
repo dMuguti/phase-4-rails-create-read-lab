0b8b8df5fb661263206e07ca7abcf4cca40a9f62
dMuguti <omiahderek56@gmail.com>
2023-06-21 09:32:14 +0300

    Done

---
+++ app/models/plant.rb
@@ -0,0 +1,5 @@
+class Plant < ApplicationRecord
+    validates :name, presence: true
+    validates :image, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+  end
